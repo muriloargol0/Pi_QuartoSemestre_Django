08f63d75354c1c3a68c22ef802419771cdebe51a
Murilo Argolo <murilo.argolo@live.com>
2021-05-25 22:39:40 -0300

    Ajustes

--- emprestei/loan/api/serializers.py
+++ emprestei/loan/api/serializers.py
@@ -4,5 +4,5 @@ from loan.models import Loan
 class LoanSerializer(ModelSerializer):
     class Meta:
         model = Loan
-        fields = ('id', 'acc_id', 'loan_to_name', 'load_date', 'loan_observation', 'loan_return_date', 'loan_estimated_value')
+        fields = ('id', 'acc_id', 'loan_to_name', 'loan_description', 'loan_date', 'loan_observation', 'loan_return_date', 'loan_estimated_value')
 	
--- emprestei/loan/models.py
+++ emprestei/loan/models.py
@@ -5,7 +5,7 @@ class Loan(models.Model):
     acc_id = models.ForeignKey(Acc, on_delete=models.RESTRICT)
     loan_to_name = models.CharField(blank=False, max_length = 200)
     loan_description = models.CharField(max_length=200, default='')
-    load_date = models.DateField(blank=False)
+    loan_date = models.DateField(blank=False)
     loan_observation = models.CharField(blank = False, max_length = 200)
     loan_return_date = models.DateField(blank=False)
     loan_estimated_value = models.DecimalField(max_digits=6, decimal_places=2)
